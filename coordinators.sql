65d5136a621cea811874c004aea0f5ffccbe0b15
Tan Jing Heng <104840897+TJH867628@users.noreply.github.com>
2026-01-06 03:44:47 +0800

    update

---
+++ coordinators.sql
@@ -0,0 +1,1 @@
+INSERT INTO `coordinators` (`id`, `department`, `createdAt`, `updatedAt`, `userID`) VALUES (1, 'FCI', 'wQwtbvXFEh', '2012-06-15 05:05:38', 3);
---
+++ evaluators.sql
@@ -0,0 +1,1 @@
+INSERT INTO `evaluators` (`id`, `expertise`, `createdAt`, `updatedAt`, `userID`) VALUES (1, 'Math', '2015-08-29 00:12:24', '2015-06-15 02:08:32', 2);
